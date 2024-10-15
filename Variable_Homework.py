20e3ba1923a189607b3bab9a09817c640cbf582d
Gil <uchihaobito9669@gmail.com>
2024-10-15 12:19:41 +0300

    15/10

---
+++ Variable_Homework.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+time_for_task = hours_count / homework_count
+name_of_course = 'Python'
+print(f"Название курса: {name_of_course}, всего задач: {homework_count}, всего часов: {hours_count}, среднее время выполнения: {time_for_task} часа")
