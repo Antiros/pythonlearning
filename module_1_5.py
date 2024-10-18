9554b97817a47e8084d302525b7eeca28a1b2fbc
Gil <uchihaobito9669@gmail.com>
2024-10-18 23:38:13 +0300

    18\10

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 3, 'Arch', True, ['apple', 1, 2, 3,] # 1
+#immutable_var[0] = 's' # 2 Кортеж является неизменяемым типом данных
+print(immutable_var)
+mutable_list = [1 ,2 ,3, 'Adrew', 1.0, False] #3
+print(mutable_list)
+mutable_list[0] = 4
+print(mutable_list)
+x = {1, 2, True, 'str'}
