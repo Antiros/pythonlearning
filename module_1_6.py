982789472c1dac2c3c69b1f3d8805f6bbc9266dd
Gil <uchihaobito9669@gmail.com>
2024-10-20 00:38:15 +0300

    19\10

---
+++ module_1_6.py
@@ -0,0 +1,27 @@
+# Словарь
+# 1
+my_dict = {'Роман' : 1999,
+           'Иван' : 2003,
+           'Олег' : 1973,
+           'Елена' : 1978,
+           'Александр' : 2012
+           }
+# 2
+print(my_dict['Олег'])
+# 3
+print(my_dict.get('Егор'))
+# 4
+my_dict.update({'Анастасия': 1952, 'Кристина': 2015})
+# 5
+name = my_dict.pop('Роман')
+print(name)
+print(my_dict)
+
+# Множества
+# 1
+my_set = {'ANT', 1, 2, 3, True, 'ANT', 1, 2, 3, True}
+print(my_set)
+# 2
+my_set.add(False)
+my_set.add((5, 2.2, 'str'))
+print(my_set)
