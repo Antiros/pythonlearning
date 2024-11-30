1f14cd5d8784e91f27e021a10be549b2eb14f82d
Gil <uchihaobito9669@gmail.com>
2024-11-30 17:25:12 +0300

    30/11

--- module_3_5.py
+++ module_3_5.py
@@ -3,6 +3,11 @@ def get_multiplied_digits(number):
     if len(str_number) <= 1:
         return int(str_number)
     first = int(str_number[0])
+    rest = int(str_number[1:])
+    if rest == 0:
+        return first
     return first * get_multiplied_digits(int(str_number[1:]))
-result = get_multiplied_digits(151519099999999990099995)
+result = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(40203)
 print(result)
+print(result2)
